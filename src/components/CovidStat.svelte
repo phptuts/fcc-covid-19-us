18666fee9088cb74c7492a22a1541b72d636a45a
Noah Glaser <glaserpower@gmail.com>
2020-04-11 21:04:25 -0700

    got covid stats component created with data

--- src/components/CovidStat.svelte
+++ src/components/CovidStat.svelte
@@ -1,1 +1,38 @@
-<h1>Covid Stat</h1>
+<script>
+  export let cases;
+  export let deaths;
+  export let recovered;
+  export let hospitalized;
+  export let ventilator;
+  export let icu;
+  export let tested;
+  export let updated;
+</script>
+
+<style>
+  .columns {
+    border: solid 1px black;
+  }
+</style>
+
+<div class="section">
+  <div class="container">
+    <div class="columns">
+      <h2 class="is-3 column">Cases: {cases}</h2>
+      <h2 class="is-3 column">Deaths: {deaths}</h2>
+      <h2 class="is-3 column">Recovered: {recovered}</h2>
+      <h2 class="is-3 column">Updated: {updated}</h2>
+    </div>
+  </div>
+</div>
+
+<div class="section">
+  <div class="container">
+    <div class="columns">
+      <h2 class="is-3 column">Tested: {tested}</h2>
+      <h2 class="is-3 column">Hospitalized: {hospitalized}</h2>
+      <h2 class="is-3 column">Total ICU: {icu}</h2>
+      <h2 class="is-3 column">Total Ventilator: {ventilator}</h2>
+    </div>
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -31,7 +31,7 @@
   </div>
 </div>
 
-<CovidStat />
+<CovidStat {...usStats} />
 
 <CovidChart />
 
